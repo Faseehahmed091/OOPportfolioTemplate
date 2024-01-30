90401b159fcecb131335ab7be885c86bf8eb706f
Faseehahmed091 <F.Ahmed2387@student.leedsbeckett.ac.uk>
2024-01-30 11:35:37 +0000

    commit

--- WeeklyExercises/1GettingStarted/PersonalDetails.java
+++ WeeklyExercises/1GettingStarted/PersonalDetails.java
@@ -1,13 +1,13 @@
-public class Personaldetails { public static void main(String[] args) {
-    // Personal details
+public class PersonalDetails { public static void main(String[] args) {
+
     String name = "Faseeh Ahmed";
     String address = "11 Savile Place,Leeds";
     int age = 21;
     String phoneNumber = "07405575481";
 
-    // Output personal details to the screen
     System.out.println("Personal Details:");
     System.out.println("Name: " + name);
     System.out.println("Address: " + address);
     System.out.println("Age: " + age);
     System.out.println("Phone Number: " + phoneNumber); }}
+
